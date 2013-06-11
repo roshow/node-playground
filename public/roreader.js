6319ac5587f7a57f7c8e68e8467621185e5585c6
roshow <rolando@roshow.net>
2013-06-11 01:19:52 -0400

    pulling in subscriptions as json now; added author to package.json

--- public/roreader.js
+++ public/roreader.js
@@ -17,70 +17,45 @@ var roreader = (function(){
 	var roread = {
 		getSubscriptions: function() {
 			var that = this;
-			var xmlhttp;
-			if (window.XMLHttpRequest) {
-				// code for IE7+, Firefox, Chrome, Opera, Safari
-				xmlhttp = new XMLHttpRequest();
-			}
-			else {
-				// code for IE6, IE5
-				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-			}
-
-			xmlhttp.onreadystatechange = function() {
-				if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-					that.feeds_display(xmlhttp.responseXML);
-					that.xmlDoc = xmlhttp.responseXML;
+			$.ajax({
+				url: "getsubs",
+				dataType: "json",
+				success: function(result){
+					that.feeds_display(result.opml.body.outline);
 				}
-			};
-			xmlhttp.open("GET", "subscriptions.xml", false);
-			xmlhttp.send();
-			currentURL = "http://roshow.net/feed/";
-			offset = 0;
-			this.getFeed_now();
+			});
 		},
-		feeds_display: function(xmlDoc) {
-			var that = this;
-			var feedTemplate = function(node) {
-				return "<div id='" + node.getAttribute('xmlUrl') + "' class='feedList_feed'>" + node.getAttribute('title') + "</div>";
+		feeds_display: function(subs) {
+			var that = this,
+			L = subs.length,
+			L2, innerHtml, html
+			var feedTemplate = function(sub) {
+				return "<div id='" + sub.xmlUrl + "' class='feedList_feed'>" + sub.title + "</div>";
 			};
-			var L = xmlDoc.childNodes[0].childNodes[3].childNodes.length;
 			for (i = 0; i < L; i++) {
-				var innerHtml = "";
-				var html = "";
-				var thisNode = xmlDoc.childNodes[0].childNodes[3].childNodes[i];
-
-				if (thisNode.nodeType === 1) {
-
-					var title = thisNode.getAttribute('title');
-
-					if (thisNode.hasChildNodes()) {
-
-						var L2 = thisNode.childNodes.length;
-						for (j = 0; j < L2; j++) {
-							var folderNode = thisNode.childNodes[j];
-							if (folderNode.nodeType === 1) {
-								innerHtml += "<span class='indent'>" + feedTemplate(folderNode) + "</span>";
-							}
-						}
-
-						html = "<div class='feedList_feed' onclick='roreader.toggleFolderFeeds($(this));'><img src='feedList_icon_folder.png' class='feedList_icon' />" + title + "</div><div style='display:none;'>" + innerHtml + "</div>";
-						$("#feedList").append(html);
-
-
+				html = '';
+				innerHtml = '';
+				if (subs[i].outline) {
+					L2 = subs[i].outline.length;
+					for (j = 0; j < L2; j++) {
+						innerHtml += "<span class='indent'>" + feedTemplate(subs[i].outline[j]) + "</span>";
 					}
-					else {
-						html = feedTemplate(thisNode);
-						$("#feedList").append(html);
-					}
+					html = "<div class='feedList_feed' onclick='roreader.toggleFolderFeeds($(this));'><img src='feedList_icon_folder.png' class='feedList_icon' />" + subs[i].title + "</div><div style='display:none;'>" + innerHtml + "</div>";
+					$("#feedList").append(html);
+				}
+				else {
+					html = feedTemplate(subs[i]);
+					$("#feedList").append(html);
 				}
 			}
 			$(".feedList_feed").click(function(){
 				console.log($(this)[0].id);
 				currentURL = $(this)[0].id;
 				offset = 0;
-				that.getFeed_now();
+				that.getFeed_now($(this)[0].id);
 			});
+			console.log('subscriptions list via json');
+			this.getFeed_now('http://roshow.net/feed');
 		},
 		toggleFolderFeeds: function(id) {
 			id.next("div").slideToggle('fast');
@@ -95,27 +70,15 @@ var roreader = (function(){
 			}
 		},
 
-		getFeed_now: function () {
+		getFeed_now: function (url) {
+			var currentURL = url || currentURL;
 			var that = this;
 			$.ajax({
-				url: "http://localhost:3000/getfeed?url=" + encodeURIComponent(currentURL),
+				url: "getfeed?url=" + encodeURIComponent(currentURL),
 				dataType: "json",
 				success: function(result){
 					that.items_display(result);
 				}
-			});
-		},
-
-		//google feed
-		getFeed_google: function() {
-			var that = this;
-			var feed = new google.feeds.Feed(currentURL);
-			feed.includeHistoricalEntries();
-			feed.setNumEntries(offset+10);
-			feed.setResultFormat(google.feeds.Feed.MIXED_FORMAT);
-			feed.load(function(result) {
-				loading = true;
-				that.items_display(result.xmlDocument);
 			});
 		}
 	};
