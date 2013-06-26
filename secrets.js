cd78fa04d6047c205015df6115e922c7a721552e
roshow <rolando@roshow.net>
2013-06-25 23:58:31 -0400

    secrets

---
+++ secrets.js
@@ -0,0 +1,13 @@
+var secrets = {
+	mongo: {
+		uri: 'mongodb://roshow:testbase@ds031978.mongolab.com:31978/rodb',
+		db: 'rodb',
+		host: 'ds031978.mongolab.com',
+		port: 31978,
+		username: 'roshow',
+		password: 'testbase'
+	},
+	session: 'rolipolizzzzz'
+}
+
+exports.secrets = secrets;
