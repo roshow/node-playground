234ee5ade4f2c9779d5e5c90ded9634749197fe6
roshow <rolando@roshow.net>
2013-07-23 01:50:46 -0400

    finessed the look and placement a bit, refactored some code, but no change to functionality

--- public/roreader.js
+++ public/roreader.js
@@ -110,26 +110,29 @@ var roreader = (function(){
 					item_btn_text = 'Mark Read';
 				}
 				html += '<div class="item_box ' + item_readStatus + '" id="' + thisItem + '">'+
-				'<h3><a href="' + items[i].link + '" target="_blank">' + items[i].title + '</a></h3>' +
-				'<p class="item_byline">Posted by ' + items[i].author + ' on '+ new Date(items[i].publishedDate).toLocaleString() + '</p>' +
-				'<br />' + 
-				content + 
-				'<br />' +
-				'<br />' +
-				'<button class="item_status_btn btn" id="' + items[i].link + '">'  + item_btn_text + '</button>' +
-				
-				//got the basic html for this from http://www.simplesharebuttons.com/html-share-buttons/ 
-				//icon images from http://www.designdeck.co.uk/a/1252
+					'<h3><a href="' + items[i].link + '" target="_blank">' + items[i].title + '</a></h3>' +
+					'<p class="item_byline">Posted by ' + items[i].author + ' on '+ new Date(items[i].publishedDate).toLocaleString() + '</p>' +
+					'<br />' + 
+					content + 
+					'<br />' +
+					'<br />' +
+								
+					//Article sharing
+					//got the basic html for this from http://www.simplesharebuttons.com/html-share-buttons/ 
+					
+					'<div id="share-buttons">' + 
+						'<a href="http://twitter.com/share?url=' + items[i].link + '&text=' + items[i].title + '" target="_blank"><img src="img/sharing/twitter.png" alt="Twitter" /></a>' +
+						'<a href="http://www.facebook.com/sharer.php?u=' + items[i].link + '"target="_blank"><img src="img/sharing/facebook.png" /></a>' +
+						'<a href="https://plus.google.com/share?url=' + items[i].link + '" target="_blank"><img src="img/sharing/google+.png" alt="Google" /></a>' +
+						'<a href="http://www.linkedin.com/shareArticle?mini=true&url=' + items[i].title + '" target="_blank"><img src="img/sharing/linkedin.png" alt="LinkedIn" /></a>' +		
+						//Pinterest ain't straight-forward <a href="javascript:void((function()%7Bvar%20e=document.createElement(\'script\');e.setAttribute(\'type\',\'text/javascript\');e.setAttribute(\'charset\',\'UTF-8\');e.setAttribute(\'src\',\'http://assets.pinterest.com/js/pinmarklet.js?r=\'+Math.random()*99999999);document.body.appendChild(e)%7D)());"><img src="http://www.simplesharebuttons.com/images/somacro/pinterest.png" alt="Pinterest" /></a>' +
+						'<a href="mailto:?Subject=' + items[i].title + '&Body=' + items[i].link + ' Sent%20from%20roreader"><img src="img/sharing/email.png" alt="Email" /></a>' +
+					'</div>' +
 
-				'<div id="share-buttons">' + 
-					'<a href="http://twitter.com/share?url=' + items[i].link + '&text=' + items[i].title + '" target="_blank"><img src="img/sharing/twitter.png" alt="Twitter" /></a>' +
-					'<a href="http://www.facebook.com/sharer.php?u=' + items[i].link + '"target="_blank"><img src="img/sharing/facebook.png" /></a>' +
-					'<a href="https://plus.google.com/share?url=' + items[i].link + '" target="_blank"><img src="img/sharing/google+.png" alt="Google" /></a>' +
-					'<a href="http://www.linkedin.com/shareArticle?mini=true&url=' + items[i].title + '" target="_blank"><img src="img/sharing/linkedin.png" alt="LinkedIn" /></a>' +		
-					//Pinterest ain't straight-forward <a href="javascript:void((function()%7Bvar%20e=document.createElement(\'script\');e.setAttribute(\'type\',\'text/javascript\');e.setAttribute(\'charset\',\'UTF-8\');e.setAttribute(\'src\',\'http://assets.pinterest.com/js/pinmarklet.js?r=\'+Math.random()*99999999);document.body.appendChild(e)%7D)());"><img src="http://www.simplesharebuttons.com/images/somacro/pinterest.png" alt="Pinterest" /></a>' +
-					'<a href="mailto:?Subject=' + items[i].title + '&Body=' + items[i].link + ' Sent%20from%20roreader"><img src="img/sharing/email.png" alt="Email" /></a>' +
-				'</div>' +
-				
+					'<br />' +
+					'<br />' +
+					'<button class="item_status_btn btn-small" id="' + items[i].link + '">'  + item_btn_text + '</button>' +
+
 				'</div>';
 
 			//add to hidden navbar for scrollspy
